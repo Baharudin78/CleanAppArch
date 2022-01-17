56bdef8a84385fc69f43c34973d7e3df5f8689b0
Baharudin78 <bencoxgame78@gmail.com>
2022-01-17 08:59:40 +0700

    session manager

---
+++ app/src/main/java/com/baharudin/cleanapparch/presentation/BaseApp.kt
@@ -0,0 +1,9 @@
+package com.baharudin.cleanapparch.presentation
+
+import android.app.Application
+
+class BaseApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
